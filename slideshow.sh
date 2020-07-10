a847e67f6096d649dba7d2a3f86ec4472066a12e
PsykeDady <psdady@msn.com>
2020-07-10 10:58:28 +0200

    slideshow adesso supporta nomi con spazio

--- slideshow.sh
+++ slideshow.sh
@@ -5,18 +5,21 @@
 ## every image in the directory you choose will renamed (one time for image) to "wall"
 
 
-cartella=/home/archdady/Immagini/slideshow #write here path of directory-slideshow
+cartella=/home/psykedady/Immagini/Wallpapers #write here path of directory-slideshow
 cd $cartella 
 
+OIFS=$IFS;
+IFS=$'\n';
 while true; do
 	for i in $(ls); do
-		if [ "$i" != "wall" ]; then
-			#echo "chang wall to $i"
-			cp -f "$i" "wall.jpg"
-			#echo "changed"
-			sleep 120 #write here number of seconds you want to wait
+		echo $i;
+		if [[ "$i" != "wall" && "$i" != "lock" ]]; then
+			echo "chang wall to $i"
+			cp -f "$i" "wall"
+			echo "changed"
+			sleep 10 #write here number of seconds you want to wait
 		fi;
 	done;
 done;
-
+IFS=$OIFS;
 
